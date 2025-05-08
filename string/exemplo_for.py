0f93dbd2eb639735b21a3031f65678b875d00c51
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-08 20:59:40 -0300

    Strings, números binãrios e menu

---
+++ string/exemplo_for.py
@@ -0,0 +1,6 @@
+frase = '''Eu só enxerguei mais longe porque me apoiei
+        em ombros de gigantes'''
+
+for letra in frase:
+    print(letra)
+
---
+++ string/menu.py
@@ -0,0 +1,20 @@
+texto = input("Digite uma frase:")
+opcao = -1
+while opcao != 5:
+    print('1) Converta a frase para maiúscula')
+    print('2) Converta a frase para minúscula')
+    print('3) Substitua a letra a pela letra o')
+    print('4) Conte a quantidade de vogais')
+    print('5) Sair')
+    opcao = int(input("Escolha: "))
+
+    if opcao == 1:
+        print(frase.upper())
+    elif opcao == 2:
+        print(frase.lower())
+    elif opcao == 3:
+        print(frase.replace('a', 'o'))
+    elif opcao == 4:
+        print("Fazer como exercicio!")
+    elif opcao != 5:
+        print("Opção inválida!")
