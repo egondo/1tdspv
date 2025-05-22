ad8f01f81ee67a916438e3ab6ab7c7a1ff2498a8
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-22 20:12:04 -0300

    projeto enquete - aplicacao

--- listas/projeto_enquete.py
+++ listas/projeto_enquete.py
@@ -3,7 +3,8 @@ def menu() -> int:
     print("1) cadastra pergunta")
     print("2) visualiza perguntas")
     print("3) apaga pergunta")
-    print("4) sair")
+    print("4) aplica perguntas")
+    print("5) sair")
     return int(input("Opção: "))
 
 def cadastra_pergunta(lista: list):
@@ -26,11 +27,36 @@ def cadastra_pergunta(lista: list):
     lista.append(tipo)
     lista.append(alternativas)
 
+def monta_alternativas(opcoes: list) -> str:
+    resp = ""
+    letras = "abcdefghijk"
+    i = 0
+    while i < len(opcoes):
+        resp = resp + f"\t{letras[i]}) {opcoes[i]}\n"   
+        i = i + 1
+    return resp
+
+def aplica_enquete(perguntas: list, respostas: list):
+    i = 0
+    while i < len(perguntas):
+        print(f"{perguntas[i]}) {perguntas[i+1]}")
+        if perguntas[i+2] != 'aberta':
+            info = monta_alternativas(perguntas[i+3])
+            print(info)
+        
+        resp = input("Resp: ")
+        respostas.append(resp)
+        i = i + 4
+
+
 
 #INICIO PROGRAMA (main)
-lista = []
+lista = [1, 'O que é Objeto?', 'aberta', None, 2, 
+    'Em Java, quais desses tipos representam números inteiros?', 'unica', 
+    ['int, short, byte', 'long, float', 'bigint, bigfloat', 'double, char'], 3, 'Assinale as alternativas incorretas?', 'multipla', ['a palavra reservada new serve para criar uma nova variavel', 'extends significa uma extensão de telefone', 'implements é um recurso da linguagem Java', 'enum define tipos fixos dentro do java', 'o construtor cria o objeto e inicializa seus atributos']]
+respostas = []
 opcao = 0
-while opcao != 4:
+while opcao != 5:
     opcao = menu()
     if opcao == 1:
         cadastra_pergunta(lista)
@@ -39,6 +65,8 @@ while opcao != 4:
     elif opcao == 3:
         print("apaga pergunta")
     elif opcao == 4:
+        aplica_enquete(lista, respostas)
+    elif opcao == 5:
         print("saindo do sistema")
     else:
         print("opcao invalida")
