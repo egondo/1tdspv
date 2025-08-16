e587cad32e515c18123c5658079cd7ac67b8d1e4
Eduardo Gondo <egondo@gmail.com>
2025-08-15 23:01:42 -0300

    Algoritmos de ordenação por inserção

--- ordenacao/insertion_sort.py
+++ ordenacao/insertion_sort.py
@@ -1,14 +1,15 @@
-def organiza(lista: list):
-    pos = len(lista) - 1
+def organiza(lista: list, pos: int):
     aux = lista[pos]
-
-    while lista[pos-1] > aux:
+    while pos > 0 and lista[pos-1] > aux:
         lista[pos] = lista[pos-1]
         pos = pos - 1
-    
     lista[pos] = aux
 
+def sort(lista: list):
+    for i in range(1, len(lista)):
+        organiza(lista, i)
+
 if __name__ == "__main__":
-    lst = [3, 5, 9, 17, 23, 30, 42, 55, 78, 21]
-    organiza(lst)
+    lst = [4, 2, 8, -1, 3, 6, 0]
+    sort(lst)
     print(lst)
