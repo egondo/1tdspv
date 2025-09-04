c637d7e13a364e58713d14722a24c3a705826dd4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-04 20:55:26 -0300

    multiplicacao matrizes - incompleto

--- matriz/multiplicacao_matriz.py
+++ matriz/multiplicacao_matriz.py
@@ -1,3 +1,11 @@
 def produto_escalar(listaA, listaB):
-    pass
+    soma = 0
+    for i in range(len(listaA)):
+        soma = soma + listaA[i] * listaB[i]
+    return soma
+
+a = [2, -5, 7]
+b = [1, 0, -1]
+resp = produto_escalar(a, b)
+print(resp)
 
