65e4ffadd4ba98dabc74ad6b89e027af6887d908
Eduardo Gondo <egondo@gmail.com>
2025-09-26 22:53:26 -0300

    Trabalhando com associacoes de tabelas no banco

--- oracle_associacao/negocio.py
+++ oracle_associacao/negocio.py
@@ -10,16 +10,19 @@ def cadastra_empresa(empresa: dict):
 
 if __name__ == "__main__":
     dado = {
-        "nome": "MOVIDA",
-        "telefone": "(11) 9823-9202",
+        "nome": "HERTZ",
+        "telefone": "(11) 7823-9743",
         "documento": "52.797.382/0001-82",
-        "url": "www.movida.com.br",
+        "url": "www.hertz.com.br",
         "veiculos": [
-            {"placa": "GWR-2I38", "montadora": "Volks", "modelo": "Golf", "ano": 2020},
-            {"placa": "KJR-0E65", "montadora": "Ford", "modelo": "Fiesta", "ano": 2019},
-            {"placa": "EHK-5F90", "montadora": "Toyota", "modelo": "Yaris", "ano": 2022}  
+            {"placa": "HWR-2I38", "montadora": "Audi", "modelo": "Q3", "ano": 2020},
+            {"placa": "KJR-0E65", "montadora": "BMW", "modelo": "325i", "ano": 2023},
+            {"placa": "EHK-5F90", "montadora": "Mercedes", "modelo": "C160", "ano": 2025}  
         ]
     }
 
     cadastra_empresa(dado)
-        
+
+    resultados = banco.consulta_tudo()
+    for info in resultados:
+        print(info)    
