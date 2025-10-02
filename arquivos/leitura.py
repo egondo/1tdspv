a87c309c7563d345299bc77d873a204c267802e1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-02 20:53:08 -0300

    Arquivos

---
+++ arquivos/leitura.py
@@ -0,0 +1,17 @@
+#USANDO READ
+
+with open('index.html', mode="r") as arquivo:
+    tudo = arquivo.read()
+
+print(tudo.replace("<p>", "<h1>"))
+
+with open('index.html', mode="r") as arq:
+    lista = arq.readlines()
+
+print(lista)
+
+with open('index.html', 'r') as file:
+    info = file.readline()
+    while info:
+        print(info)
+        info = file.readline()
---
+++ arquivos/teste_autogui.py
@@ -0,0 +1,4 @@
+import pyautogui
+
+pyautogui.moveTo(120, 690, duration=5)
+pyautogui.click()
